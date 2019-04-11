8d1662008986e71829876858608b51182382f4a8
Aaron Nicolson <aaron.nicolson@griffithuni.edu.au>
2019-04-12 08:19:55 +1000

    Add license.

--- lib/dct.py
+++ lib/dct.py
@@ -1,8 +1,23 @@
 ## FILE:           dct.py 
-## DATE:           2018
+## DATE:           2019
 ## AUTHOR:         Aaron Nicolson
 ## AFFILIATION:    Signal Processing Laboratory, Griffith University.
 ## BRIEF:          Short-time Discrete Cosine Transform (STDCT). DCT Type-II is used.
+##
+## Copyright (C) 2019  Aaron Nicolson
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import functools
 from tensorflow.contrib.signal.python.ops import shape_ops
