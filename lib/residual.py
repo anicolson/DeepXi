2260215c56b2691fd8ef7cfdd29ed559698ddaa3
Aaron Nicolson <aaron.nicolson@griffithuni.edu.au>
2019-04-12 08:36:26 +1000

    Add license.

--- lib/residual.py
+++ lib/residual.py
@@ -4,20 +4,9 @@
 ## AFFILIATION:    Signal Processing Laboratory, Griffith University.
 ## BRIEF:          Functions to create neural neworks with residual links.
 ##
-## Copyright (C) 2019  Aaron Nicolson
-##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+## This Source Code Form is subject to the terms of the Mozilla Public
+## License, v. 2.0. If a copy of the MPL was not distributed with this
+## file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import tensorflow as tf
 from tensorflow.python.training import moving_averages
