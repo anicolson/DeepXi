d65a0f210023d09deb4c4ee40bfb38c493351ec4
Aaron Nicolson <aaron.nicolson@griffithuni.edu.au>
2020-08-13 11:13:15 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -93,6 +93,47 @@ then
                     --model_path        $MODEL_PATH
 fi
 
+if [ "$VER" == 'rdlnet-1.0n' ]
+then
+    python3 main.py --ver               $VER                        \
+                    --network           'RDLNet'                    \
+                    --n_blocks          10                          \
+                    --length            7                           \
+                    --m_1               64                          \
+                    --causal            0                           \
+                    --unit_type         "ReLU->scale*LN+center->W+b"\
+                    --loss_fnc          "BinaryCrossentropy"        \
+                    --max_epochs        200                         \
+                    --resume_epoch      0                           \
+                    --test_epoch        180                         \
+                    --mbatch_size       8                           \
+                    --inp_tgt_type      'MagXi'                     \
+                    --xi_map_type       'DBNormalCDF'               \
+                    --sample_size       1000                        \
+                    --f_s               16000                       \
+                    --T_d               32                          \
+                    --T_s               16                          \
+                    --min_snr           -10                         \
+                    --max_snr           20                          \
+                    --snr_inter         1                           \
+                    --out_type          $OUT_TYPE                   \
+                    --save_model        1                           \
+                    --log_iter          0                           \
+                    --eval_example      1                           \
+                    --gain              $GAIN                       \
+                    --train             $TRAIN                      \
+                    --infer             $INFER                      \
+                    --test              $TEST                       \
+                    --gpu               $GPU                        \
+                    --set_path          $SET_PATH                   \
+                    --data_path         $DATA_PATH                  \
+                    --test_x_path       $TEST_X_PATH                \
+                    --test_s_path       $TEST_S_PATH                \
+                    --test_d_path       $TEST_D_PATH                \
+                    --out_path          $OUT_PATH                   \
+                    --model_path        $MODEL_PATH
+fi
+
 # if [ "$VER" == 'resnet-1.0c' ]
 # then
 #     python3 main.py --ver               'resnet-1.0c'   \
